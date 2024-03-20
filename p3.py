f46f31db46cca2cfec16c0dcc2c3536f0337ada9
divu76 <136615792+divu76@users.noreply.github.com>
2024-03-20 22:13:41 +0530

    Add files via upload

---
+++ p3.py
@@ -0,0 +1,10 @@
+# Basic Calculator 
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+print("Addition:", num1 + num2) 
+print("Subtraction:", num1 - num2)
+print("Multiplication:", num1 * num2) 
+print("Division:", num1 / num2)
+print("Modulus:", num1 % num2) 
+print("Floor Division:", num1 // num2)
+print("Exponential:", num1 ** num2)
