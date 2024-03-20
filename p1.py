2f3e6cd1998cccc3a09b29ce6492dfd94b541a10
divu76 <136615792+divu76@users.noreply.github.com>
2024-03-20 22:12:53 +0530

    Add files via upload

---
+++ p1.py
@@ -0,0 +1,7 @@
+userReply = input("Do you need to ship a package? (Enter yes or no) ")
+if userReply =="yes":
+     print("we can help you ship that package") 
+elif userReply=="no": 
+     print("Please come back when you need to ship a package. Thank you.")
+else:
+      print("please specify your answer")
